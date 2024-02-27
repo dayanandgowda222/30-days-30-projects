14e62eeee294a4ccfe3e7fe90fe46e282edab2d1
Dayananda Gowda I J <dayanandagowda2002@gmail.com>
2024-02-28 00:35:50 +0530

    Add files via upload

---
+++ Project-7( Pop up Message )/script.js
@@ -0,0 +1,11 @@
+let popup = document.getElementById("popup");
+/*console.log(popup)*/
+
+  function openPopup(){
+      popup.classList.add("open_popup");
+      console.log('daya')
+  }
+
+  function closePopup(){
+      popup.classList.remove('open_popup')
+  }
